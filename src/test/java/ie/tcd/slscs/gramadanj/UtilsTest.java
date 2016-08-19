59dbe4d4cfa3d1fe2dd7cc3d4115a2232cd14a01
Jim O'Regan <joregan@gmail.com>
2016-08-19 17:06:08 +0100

    dunno why this fails

--- src/test/java/ie/tcd/slscs/gramadanj/UtilsTest.java
+++ src/test/java/ie/tcd/slscs/gramadanj/UtilsTest.java
@@ -1,4 +1,35 @@
 package ie.tcd.slscs.gramadanj;
 
-public class UtilsTest {
+import junit.framework.TestCase;
+import ie.tcd.slscs.gramadanj.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+public class UtilsTest extends TestCase {
+    public void testEqualLists() {
+        List<String> a = new ArrayList<String>();
+        List<String> b = new ArrayList<String>();
+        a.add("bee");
+        a.add("cee");
+        a.add("ay");
+        b.add("cee");
+        b.add("ay");
+        b.add("bee");
+        assertEquals(true, Utils.equalLists(a, b));
+    }
+
+    public void testEqualsListsWithForm() {
+        List<Form> a = new ArrayList<Form>();
+        List<Form> b = new ArrayList<Form>();
+        a.add(new Form("bee"));
+        a.add(new Form("cee"));
+        a.add(new Form("ay"));
+        b.add(new Form("cee"));
+        b.add(new Form("ay"));
+        b.add(new Form("bee"));
+        assertEquals(true, Utils.equalLists(a, b));
+    }
 }
