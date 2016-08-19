6281c5cdf3ba5d37d5cba73e4fa55dc62387d906
Jim O'Regan <joregan@gmail.com>
2016-08-19 17:33:02 +0100

    tabs

--- src/main/java/ie/tcd/slscs/gramadanj/FormSg.java
+++ src/main/java/ie/tcd/slscs/gramadanj/FormSg.java
@@ -2,19 +2,19 @@ package ie.tcd.slscs.gramadanj;
 import ie.tcd.slscs.gramadanj.Form;
 
 public class FormSg extends Form implements Comparable<Form> {
-	public Gender gender;
-	FormSg (String value, Gender gender) {
-		super(value);
-		this.gender=gender;
-	}
-	public boolean equals(FormSg f) {
-		if(f != null && (f instanceof FormSg)) {
-			return f.value.equals(value) && f.gender == gender;
-		} else {
-			return false;
-		}
-	}
-	public int hashCode() {
-		return value.hashCode();
-	}
+    public Gender gender;
+    FormSg (String value, Gender gender) {
+        super(value);
+        this.gender=gender;
+    }
+    public boolean equals(FormSg f) {
+        if(f != null && (f instanceof FormSg)) {
+            return f.value.equals(value) && f.gender == gender;
+        } else {
+            return false;
+        }
+    }
+    public int hashCode() {
+        return value.hashCode();
+    }
 }
