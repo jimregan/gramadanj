37891e75129ec1a7ccd3a9c504f958549c4db09d
Jim O'Regan <joregan@gmail.com>
2016-09-26 17:23:29 +0100

    port constructor

--- src/main/java/ie/tcd/slscs/gramadanj/VP.java
+++ src/main/java/ie/tcd/slscs/gramadanj/VP.java
@@ -8,6 +8,7 @@ package ie.tcd.slscs.gramadanj;
  * http://creativecommons.org/licenses/by/4.0/
  */
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,8 +52,58 @@ public class VP {
 
     public Map<VPTense, Map<VPShape, Map<VPPerson, Map<VPPolarity, List<Form>>>>> tenses;
     public Map<VPMood, Map<VPPerson, Map<VPPolarity, List<Form>>>> moods;
-    VP() {
+    public VP() {
         tenses = new HashMap<VPTense, Map<VPShape, Map<VPPerson, Map<VPPolarity, List<Form>>>>>();
         moods = new HashMap<VPMood, Map<VPPerson, Map<VPPolarity, List<Form>>>>();
+        for(VPTense t : VPTense.values()) {
+            this.tenses.put(t, new HashMap<VPShape, Map<VPPerson, Map<VPPolarity, List<Form>>>>());
+            for(VPShape s : VPShape.values()) {
+                this.tenses.get(t).put(s, new HashMap<VPPerson, Map<VPPolarity, List<Form>>>());
+                for(VPPerson p : VPPerson.values()) {
+                    this.tenses.get(t).get(s).put(p, new HashMap<VPPolarity, List<Form>>());
+                    for(VPPolarity pol : VPPolarity.values()) {
+                        this.tenses.get(t).get(s).get(p).put(pol, new ArrayList<Form>());
+                    }
+                }
+            }
+        }
+        for(VPMood m : VPMood.values()) {
+            this.moods.put(m, new HashMap<VPPerson, Map<VPPolarity, List<Form>>>());
+            for(VPPerson p : VPPerson.values()) {
+                this.moods.get(m).put(p, new HashMap<VPPolarity, List<Form>>());
+                for(VPPolarity pol : VPPolarity.values()) {
+                    this.moods.get(m).get(p).put(pol, new ArrayList<Form>());
+                }
+            }
+        }
+    }
+    public VP(Verb v) {
+        this();
+        for(VPTense t : v.tenseRules.keySet()) {
+            for(VPPerson p : v.tenseRules.get(t).keySet()) {
+                for(VPShape s : v.tenseRules.get(t).get(p).keySet()) {
+                    for(VPPolarity pol : v.tenseRules.get(t).get(p).get(s).keySet()) {
+                        for(VerbTenseRule r : v.tenseRules.get(t).get(p).get(s).get(pol)) {
+                            for(Form f : v.tenses.get(r.verbTense).get(r.verbDependency).get(r.verbPerson)) {
+                                Form tmp = new Form("");
+                                if(!"".equals(r.particle)) {
+                                    tmp.value = r.particle + " ";
+                                }
+                                tmp.value += Opers.Mutate(r.mutation, f.value);
+                                if(!"".equals(r.pronoun)) {
+                                    tmp.value += " " + r.pronoun;
+                                }
+                                if(v.getLemma().equals("bí")
+                                        && t == VPTense.Pres && s == VPShape.Declar
+                                        && pol == VPPolarity.Neg && tmp.value.startsWith("ní fhuil")) {
+                                    tmp.value = "níl" + tmp.value.substring(8);
+                                }
+                                this.tenses.get(t).get(s).get(p).get(pol).add(tmp);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
