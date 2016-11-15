747c147aa860b80c879f42cf25a47921e7b44b6e
Jim O'Regan <joregan@gmail.com>
2016-11-15 15:08:34 +0000

    start of pieces to split/normalise regular labels; need to do something similar in repair, to split PoS from domain

--- src/main/java/ie/tcd/slscs/itut/gramadanj/EID/LabelMap.java
+++ src/main/java/ie/tcd/slscs/itut/gramadanj/EID/LabelMap.java
@@ -1,5 +1,6 @@
 package ie.tcd.slscs.itut.gramadanj.EID;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,13 @@ import java.util.Map;
  * Created by jaoregan on 14/11/2016.
  */
 public class LabelMap {
+    final static Map<String, String[]> multi;
+    static {
+        Map<String, String[]> multitmp = new HashMap<String, String[]>();
+        multitmp.put("Ecc.Arch", new String[] {"Ecc", "Arch"});
+        multitmp.put("Ecc.", new String[] {"Ecc"});
+        multi =  Collections.unmodifiableMap(multitmp);
+    }
     private Map<String, String> wdtmp = new HashMap<String, String>();
     private Map<String, String> fbtmp = new HashMap<String, String>();
     private Map<String, String> gatmp = new HashMap<String, String>();
@@ -21,5 +29,10 @@ public class LabelMap {
         addEntry("Pol", "Q7163", "/m/05qt0", "Polaitíocht", "Politics");
         addEntry("Ch", "Q2329", "/m/01lj9", "Ceimic", "Chemistry");
         addEntry("Ph", "Q413", "/m/05qjt", "Fisic", "Physics");
+        addEntry("Arch", "Q12271", "/m/03nfmq", "Ailtireacht", "Architecture");
     }
+    String[] fixMultipartTags(String s) {
+        return multi.get(s);
+    }
+
 }
