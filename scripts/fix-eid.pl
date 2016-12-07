7e6ceee10027ceeff6af41970239bc840fd29373
Jim O'Regan <joregan@gmail.com>
2016-12-07 12:19:27 +0000

    generalise

--- scripts/fix-eid.pl
+++ scripts/fix-eid.pl
@@ -6,7 +6,7 @@ use strict;
 while(<>) {
     s#</src>, a<label>\.</label>#</src>, <label>a.</label>#;
     s#<label>Déanaim amas</label>#<trg>Déanaim amas</trg>#;
-    s#<label>m -a</label></trg>#<label>m</label> -a</trg>#;
+    s#<label>([mf]) -([^<]*)</label></trg>#<label>$1</label> -$2</trg>#;
 
     s#etc</trg>.\).#etc.)</trg>.#;
     s# <noindex>\(<label>([^<]*)</label>, ([^)]*)\)</noindex></trg>#</trg> <noindex>(<src>$1</src>, <trg>$2</trg>)</noindex>#;
