6d68b27c241a3b14c31c987b1a6092198adf1d91
Jim O'Regan <joregan@gmail.com>
2017-04-21 12:49:05 +0100

    new

---
+++ src/main/java/ie/tcd/slscs/itut/gramadanj/FGB/B.java
@@ -0,0 +1,80 @@
+package ie.tcd.slscs.itut.gramadanj.FGB;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2015-2017 Trinity College, Dublin
+ * Irish Speech and Language Technology Research Centre
+ * Cóipcheart © 2015-2017 Coláiste na Tríonóide, Baile Átha Cliath
+ * An tIonad taighde do Theicneolaíocht Urlabhra agus Teangeolaíochta na Gaeilge
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import ie.tcd.slscs.itut.gramadanj.Utils;
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The &lt;b&gt; element when following &lt;g&gt; contains a word form, or
+ * information from which to construct one.
+ * In other contexts, it can contain a subsense number
+ */
+public class B {
+    private String raw;
+    private String text;
+    private boolean grammar;
+
+    public boolean isGrammar() {
+        return grammar;
+    }
+
+    public boolean isEquals() {
+        return equals;
+    }
+
+    public void setEquals(boolean equals) {
+        this.equals = equals;
+    }
+
+    private boolean equals;
+
+    B(String s) {
+        this.raw = s;
+        String clean = Utils.cleanTrailingPunctuation(Utils.trim(s));
+        if(clean.charAt(clean.length()-1) == '=') {
+            this.equals = true;
+        }
+    }
+    public void setGrammar(boolean gram) {
+        this.grammar = gram;
+    }
+    public static B fromNode(Node n, boolean grammar) throws Exception {
+        String txt;
+        if(n.getNodeName().equals("b")) {
+            txt = n.getFirstChild().getTextContent();
+        } else {
+            throw new Exception("Unexpected node: " + n.getNodeName());
+        }
+        B ret = new B(txt);
+        ret.setGrammar(grammar);
+        return ret;
+    }
+}
