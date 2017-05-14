c595fa67f89c360ab067aa4683e9039e85609a60
Jim O'Regan <joregan@gmail.com>
2017-05-14 13:05:31 +0100

    missing imports

--- src/main/java/ie/tcd/slscs/itut/gramadanj/Utils.java
+++ src/main/java/ie/tcd/slscs/itut/gramadanj/Utils.java
@@ -34,7 +34,9 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Utils {
 
